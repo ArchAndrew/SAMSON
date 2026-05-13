0cc586b7b12d7eaebc74ebd081068164a4690ac5
ArchAndrew <a.bandoo3733@yahoo.com>
2026-05-13 00:53:19 -0400

    feat(terraform): implement enterprise tagging standards

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,14 +1,20 @@
 module "aws_organizations_core" {
   source = "../../modules/aws-organizations-core"
 }
+module "tagging_standards" {
+  source = "../../modules/tagging-standards"
+
+  project             = "SAMSON"
+  environment         = "dev"
+  owner               = "thee_architect_was_here"
+  cost_center         = "SecurityEngineering"
+  data_classification = "Confidential"
+  compliance_scope    = "HIPAA-NIST-800-53-ISO-27001"
+}
 
 module "backend_bootstrap" {
   source      = "../../modules/backend-bootstrap"
   bucket_name = "samson-terraform-state-14412"
 
-  tags = {
-    Project     = "SAMSON"
-    Environment = "dev"
-    Owner       = "thee_architect_was_here"
-  }
+  tags = module.tagging_standards.merged_tags
 }
